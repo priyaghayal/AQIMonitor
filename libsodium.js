ed778ffb45fcad5b58458f65ed3a1cbf35c5dc35
priyaghayal <53147571+priyaghayal@users.noreply.github.com>
2025-04-23 19:17:12 +0200

    Update libsodium.js

--- libsodium.js
+++ libsodium.js
@@ -7,7 +7,7 @@ import sodium from 'libsodium-wrappers';
 const dbConfig = {
     host: 'localhost',
     user: 'root',
-    password: 'Priya@1234',
+    password: '*********",  //Replace with your own Mysql root password
     database: 'SensorData'
 };
 
